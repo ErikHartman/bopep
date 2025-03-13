7aac00db79e271f38ce0df059024b630d31eae71
Malcom Siljehag Alencar <malcolm@alencar.se>
2025-03-13 14:44:54 +0100

    Added Docker to __init__.py file

--- bopep/__init__.py
+++ bopep/__init__.py
@@ -1,3 +1,4 @@
 from bopep.bayesian_optimization.optimization import BoPep
 from bopep.scoring.scorer import Scorer
 from bopep.embedding.embedder import Embedder
+from bopep.docking.docker import Docker
