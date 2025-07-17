2400e0926eee93465d9f82168ecec1b2202c34a5
ErikHartman <erik.hartman@hotmail.com>
2025-07-17 14:47:07 +0200

    Add mps in embedder

--- bopep/embedding/embed_esm.py
+++ bopep/embedding/embed_esm.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 import torch
 import esm
 from pathlib import Path
+from typing import Optional
 
 
 def embed_esm(
@@ -10,8 +11,22 @@ def embed_esm(
     model_path="esm2_t33_650M_UR50D.pt",
     average: bool = True,
     batch_size: int = 128,
+    device: Optional[str] = None,
 ):
-    # Check if the model file exists locally
+    # Determine the device
+    if device:
+        device = device
+    elif torch.cuda.is_available():
+        device = "cuda"
+        print("ESM will use CUDA.")
+    elif torch.backends.mps.is_available():  # Apple Silicon
+        device = "mps"
+        print("ESM will use MPS (Apple Silicon GPU).")
+    else:
+        device = "cpu"
+        print("GPU not available. ESM will use CPU.")
+
+    # Load the model
     if model_path:
         model_path = os.path.abspath(model_path)
         if os.path.exists(model_path):
@@ -21,16 +36,14 @@ def embed_esm(
             model, alphabet = esm.pretrained.load_model_and_alphabet_core(
                 model_name, model_data
             )
+        else:
+            raise FileNotFoundError(f"Specified model path does not exist: {model_path}")
     else:
-        # Download the model if it does not exist locally
         model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
 
-    # Move model to GPU if available
-    if torch.cuda.is_available():
-        model = model.to("cuda")
-        print("ESM moved to GPU.")
-    else:
-        print("GPU not available. ESM remains on CPU.")
+    # Move model to chosen device
+    model = model.to(device)
+    print(f"ESM moved to {device}.")
 
     batch_converter = alphabet.get_batch_converter()
     model.eval()  # Set the model to evaluation mode
@@ -44,8 +57,8 @@ def embed_esm(
         batch_sequences = sequence_data[i : i + batch_size]
         batch_labels, batch_strs, batch_tokens = batch_converter(batch_sequences)
 
-        # Move input tokens to GPU if available
-        batch_tokens = batch_tokens.to("cuda" if torch.cuda.is_available() else "cpu")
+        # Move input tokens to the same device as the model
+        batch_tokens = batch_tokens.to(device)
 
         # Generate embeddings with no gradient tracking
         with torch.no_grad():
@@ -65,6 +78,6 @@ def embed_esm(
             else:
                 embedding = token_embeddings[j, 1 : seq_len + 1].cpu().numpy()
             embeddings[seq] = embedding
-            
+
     print("ESM embedding dim: ", embedding.shape)
     return embeddings
