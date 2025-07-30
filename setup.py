91b6922285fb9877ced7127daded34087043dfa8
Malcolm Alencar <malcolm@alencar.se>
2025-07-30 16:00:53 +0200

    Added pyrosetta as an optional dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,12 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=requirements,
+    extras_require={
+        "pyrosetta": [
+            "pyrosetta @ https://west.rosettacommons.org/pyrosetta/release/release/PyRosetta4.Release.python313.ubuntu.wheel/pyrosetta-2025.25+release.a0cefad01b-cp313-cp313-linux_x86_64.whl#sha256=0ec84ac865336ca3266eb8a6e8b1c5908dbce661db21f173bbed36007e3f8408",
+            "pyrosetta-installer==0.1.2"
+        ],
+    },
     author="Erik Hartman",
     author_email="erik.hartman@hotmail.com",
     description="Bayesian Optimization for peptide docking",
