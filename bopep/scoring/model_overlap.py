534d8ec5e723a56bc0c57738505faa678e1e0a89
ErikHartman <erik.hartman@hotmail.com>
2025-09-17 16:57:57 +0200

    removed utisl

--- bopep/scoring/model_overlap.py
+++ bopep/scoring/model_overlap.py
@@ -1,10 +1,20 @@
 import numpy as np
 from Bio.SVDSuperimposer import SVDSuperimposer
-from bopep.scoring.utils import match_and_truncate
 from bopep.structure.parser import get_chain_sequences, get_chain_coordinates
 import os
 import glob
 from itertools import combinations
+
+
+def match_and_truncate(ref_seq :  str, ref_coords : list, target_seq : str, target_coords : list):
+    if ref_seq in target_seq:
+        i = target_seq.index(ref_seq)
+        return ref_coords, target_coords[i:i+len(ref_seq)]
+    elif target_seq in ref_seq:
+        i = ref_seq.index(target_seq)
+        return ref_coords[i:i+len(target_seq)], target_coords
+    else:
+        raise ValueError(f"Could not match reference and target receptor sequences for alignment. Reference sequence: {ref_seq}, Target sequence: {target_seq}")
 
 def rmsd(coords1 : np.ndarray, coords2 : np.ndarray) -> float:
     return np.sqrt(np.mean(np.sum((coords1 - coords2) ** 2, axis=1)))
